45d02a7ca776674fbaa4df382e86d870d9db4fbe
wangyang <yang1.wang@baidao.com>
2017-06-28 16:41:05 +0800

    Publish Blog at 2017年 6月28日 星期三 16时41分05秒 CST

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 jekyll build --destination ../SquarePants1991.github.io
+cp ./_config.yml ../SquarePants1991.github.io
 cd ../SquarePants1991.github.io
 git add .
 git commit -m "Publish Blog at $(Date)"
