894e5f628ca9a03eed9e030658e4a49361d467c4
wangyang <yang1.wang@baidao.com>
2017-07-07 14:06:07 +0800

    update at 2017年 7月 7日 星期五 14时06分07秒 CST

--- jianshutask.py
+++ jianshutask.py
@@ -7,14 +7,14 @@ import jianshusummary
 import subprocess
 
 subprocess.call(['osascript', '-e', 'display notification "日常任务后台已启动" with title "日常任务"'])
-if jianshusummary.processJianshuSummary():
-    subprocess.call(['osascript', '-e', 'display notification "成功刷新简书数据" with title "日常任务"'])
-    subprocess.call(['zsh', './publish.sh'])
+
 # 输出时间
 def job():
-    # subprocess.call(['osascript', '-e', 'display notification "正在刷新简书数据" with title "日常任务"'])
-    pass
+    subprocess.call(['osascript', '-e', 'display notification "正在刷新简书数据" with title "日常任务"'])
+    if jianshusummary.processJianshuSummary():
+        subprocess.call(['osascript', '-e', 'display notification "成功刷新简书数据" with title "日常任务"'])
+        subprocess.call(['zsh', './publish.sh'])
 # BlockingScheduler
 scheduler = BlockingScheduler()
-scheduler.add_job(job, 'cron', day_of_week='1-7', hour=14, minute=3)
+scheduler.add_job(job, 'cron', day_of_week='1-7', hour=14, minute=6)
 scheduler.start()
