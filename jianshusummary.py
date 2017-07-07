e0f691dcf4d2f4621ea2a43c80695d1b9d3f20d4
wangyang <yang1.wang@baidao.com>
2017-07-07 14:05:21 +0800

    update at 2017年 7月 7日 星期五 14时05分21秒 CST

--- jianshusummary.py
+++ jianshusummary.py
@@ -1,6 +1,7 @@
 import httplib
 from bs4 import BeautifulSoup
 import json
+import datetime
 
 urlTemplate = '/u/e6367cf15710?order_by=shared_at&page={0}'
 cookie = 'remember_user_token=W1syOTQ5NzUwXSwiJDJhJDEwJDdoOVlBRkhwSk9nMXdLanliSUNNSWUiLCIxNDk5MTQ3MTU5Ljg1ODcwNDMiXQ%3D%3D--7c5dc200db966f1d819e4c5e2650d8cc5f49a05e; _ga=GA1.2.669366992.1497926671; _gid=GA1.2.34677441.1499066591; _gat=1; Hm_lvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1498703353,1499066591,1499135531,1499135540; Hm_lpvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1499311733; _session_id=RTROUDJibXlnU0xyTnZQNEp0OUFBbXFYdUN6N3A1NitTUGR0N2t4dmNtMXFWcU9nanh1aVFtRTZ2RGV0b3ZmeG8yamkzd1ArNVdySG9zVXhFb2pJUXBabWc1K09xblU1NjdEelpCSFJuVHkxZ3ErK21PUi9lSXcwaUNTUDNRVVU5WXdESkJaODVZd0ZLMWNMMTVDbkxFLzhxaTdCVUk0UGZ4MUhFMDBjSHhjM3FHWWcxWUNycnk2RUt2OGJsY2doMjNPYWdUMlVQb3luMGhvemN0aEpxTnVqYlpCM1FmWk1vU2s4d3pkdnA3L2FsQzBuRmp1N2Z3bmFnYUV5SVF5cTJPYlEvSlRsUmdVSFNkQkRWdjkxRHdyQUZQbTh0SGpCemRWSkNIWVZwRGt3WERiRWZsK0FsS3FxREtTbTBjT0c1TWtGTUdKSHREZ3E0ZFgvcTlqNkcyN1V1Wk5MQ1NDeDRuOVlhQ0FmVmVWT3BGbFQ5TGtRQkNSc1FYc3ZsRkZORWwvRHIxditpMDJlN3ZxMDh3N3ZkT3UzR1AwbTJXcVZ6WEd6SHB0ZGtEbz0tLVNheUczZzU5WWFZQkQvT2cyazRLWmc9PQ%3D%3D--1b3fbe3886b77020dae65067dbca59215f5f3939; remember_user_token=W1syOTQ5NzUwXSwiJDJhJDEwJDdoOVlBRkhwSk9nMXdLanliSUNNSWUiLCIxNDk5MTQ3MTU5Ljg1ODcwNDMiXQ%3D%3D--7c5dc200db966f1d819e4c5e2650d8cc5f49a05e; _session_id=Tk81cXRsUXA1TExXcW1uazZZNzVsT1h5ZkN6WVAzbHJiTkdRbkh5dmpUWHMxZllqaTgxd3pIcHA2QkVJMG9IYkZhcG1HSUxidlgvOXV3MVJQNGtJeml1TVg4dkZvZWJNZnV5ZjYwUDE0OE5XeTFnM2FHNW54Qm9GOVM1cnd6bW81bUJ6TXo5R3lQdGwzT29iUUZRUFhQUGd1OXRQVEh1L2hNNDl0dDlhOVU1QURuNG5HZkhPNFFKNVdsU0Nqam94STZReXdGdGgxNlVNcWt5TTE1OEV4ejdsWlF3UUNxTHM2NkxUZ1ZwbTFYSUF2Q1RwTmgxNzFOOTdmbkdrZVJzSHp2V3NWb0VPWTIrbVhiTGNWNEY2ZzY2eVd1QzRSMzBMWVd2dTdocHJMaWpqM2dnZWE3dHVrdjYzeVNIYzc5bjNsODVZTTdRcER2Y05kaHh2Ryt1U1pKeE5pZUFnYnc0OHNhVnExekpaL3pvRzNMaExzNVRQSzc3aHVQT21IVGQ2N1pSQ2lqN3BJZVhRbWhuekdJa0p4ckdqOGRGNVJ5RGFWM3djVHZ5RzMyWT0tLWQ3MkVjRC9yQTNucTI4dHFQdkg0cXc9PQ%3D%3D--fd2d4e8f24bdb9cd731b223d143e9e484eb02b87; _ga=GA1.2.669366992.1497926671; _gid=GA1.2.34677441.1499066591; Hm_lvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1498703353,1499066591,1499135531,1499135540; Hm_lpvt_0c0e9d9b1e7d617b3e6842e85b9fb068=1499319512 If-None-Match: W/"30c2e128a60740cd508d2e3fd1252865"'
@@ -28,7 +29,7 @@ def requestAll():
         soup = BeautifulSoup(html, 'html.parser')
         for item in soup.find_all('li'):
             article = {}
-            article['title'] = item.find_all('a', 'title')[0].string
+            article[' title'] = item.find_all('a', 'title')[0].string
             article['readCount'] = int(item.find_all('i', 'iconfont ic-list-read')[0].next_sibling)
             article['commentCount'] = int(item.find_all('i', 'iconfont ic-list-comments')[0].next_sibling)
             article['loveCount'] = int(item.find_all('i', 'iconfont ic-list-like')[0].next_sibling)
@@ -37,9 +38,14 @@ def requestAll():
         index += 1
     return articles
 
-articles = requestAll()
-jsonStr = json.dumps(articles)
+def processJianshuSummary():
+    articles = requestAll()
+    if len(articles) == 0:
+        return False
+    jsonStr = json.dumps(articles)
 
-with open('./public/summary.json', 'w') as file:
-    file.write(jsonStr)
+    filename = str.format('./public/jianshu-summary/{0}.json', datetime.datetime.today().strftime('%Y-%m-%d'))
+    with open(filename, 'w') as file:
+        file.write(jsonStr)
+    return True
 
---
+++ jianshutask.py
@@ -0,0 +1,20 @@
+#!env python
+# -*- coding: UTF-8 -*-
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import jianshusummary
+import subprocess
+
+subprocess.call(['osascript', '-e', 'display notification "日常任务后台已启动" with title "日常任务"'])
+if jianshusummary.processJianshuSummary():
+    subprocess.call(['osascript', '-e', 'display notification "成功刷新简书数据" with title "日常任务"'])
+    subprocess.call(['zsh', './publish.sh'])
+# 输出时间
+def job():
+    # subprocess.call(['osascript', '-e', 'display notification "正在刷新简书数据" with title "日常任务"'])
+    pass
+# BlockingScheduler
+scheduler = BlockingScheduler()
+scheduler.add_job(job, 'cron', day_of_week='1-7', hour=14, minute=3)
+scheduler.start()
